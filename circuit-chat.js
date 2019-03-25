e008663bb78bd485a709b7c1b65ea44b52b07046
Fazzio <richard.fazzio@unify.com>
2019-03-25 11:50:30 -0400

    Add Custom element for circuit-chat

--- circuit-chat.js
+++ circuit-chat.js
@@ -1,6 +1,11 @@
 const template = document.createElement('template');
 const initHtml = `
 <style>
+hr {
+  border: 0;
+  margin: 0;
+  padding-top: 3px;
+}
 .name {
   align-self: flex-start;
   min-width: var(--name-width);
@@ -37,29 +42,31 @@ export class CircuitConversation extends HTMLElement {
     }
   }
 
-  // Get the conversation here
   set convId(value) {
     // If no value given or if the conversation is the same as before
-    if (!value || !value.length || value === this._convId) {
+    if (!value || value === this._convId) {
         return;
     }
     this._resetValues();
     this._convId = value;
-    // Reflect to attribute
     this._conversationFeed.display = 'none';
     this._input.display = 'none';
+    // Reflect to attribute
     this.setAttribute('convId', value);
-    this._getConversation();
+    this._getItems();
   }
 
-  set maxItems(value) {
-    // Max size of conversations store at once, defaults to 100
-    this._maxItems = value ? value : 100;
+  set sendOnEnter(value) {
+    this._sendOnEnter = !!value;
   }
 
   set initNumOfItems(value) {
     // Initial number of items to retrieve, defaults to 20
     this._initNumOfItems = value ? value : 20;
+  }
+
+  get sendOnEnter() {
+    return this._sendOnEnter;
   }
 
   get convId() {
@@ -87,25 +94,18 @@ export class CircuitConversation extends HTMLElement {
   // Delay Circuit initialization to speed up page load. This way the SDK
   // can be loaded with 'async'
   async _init() {
-    // this._conversationFeed.style.overflowY = 'auto';
     this._client = Circuit.Client({
       domain: this._domain,
       client_id: this._clientId
     });
 
-    // Replacing hr from circuit items to br
-    Circuit.Injectors.itemInjector = function (item) {
-      if (item.type === Circuit.Enums.ConversationItemType.TEXT) {
-        // Replacing br and hr tags with a space
-        item.text.content = item.text.content.replace(/<(\/li|hr[\/]?)>/gi, '<br>');
-      }
-    };
     // Event listeners
 
     // Send a message
     this._btn.addEventListener('click', () => this._sendTextItem());
     this._input.addEventListener('keyup', evt => {
-      if (evt.keyCode === 13) {
+      // Key Code 13 is ENTER
+      if (this._sendOnEnter && evt.keyCode === 13) {
         this._sendTextItem();
       }
     });
@@ -121,14 +121,8 @@ export class CircuitConversation extends HTMLElement {
         this._usersHashMap[item.creatorId] = await this._client.getUserById(item.creatorId);
       }
       this._feed.push(item);
-      if (this._feed.length > this._maxItems) {
-        const itemId = this._feed[0].itemId;
-        this._feed.splice(0,1);
-        let child = this.root.getElementById(itemId);
-        this._conversationFeed.removeChild(child);
-      }
       this._conversationFeed.appendChild(this._createItemHtml(item));
-      this.root.getElementById(item.itemId).scrollIntoView();
+      this._focusFeed();
     });
 
     // An item in the feed was updated
@@ -140,24 +134,11 @@ export class CircuitConversation extends HTMLElement {
       let oldItem = this.root.getElementById(item.itemId);
       if (oldItem) {
         oldItem.innerHTML = this._createItemHtml(item).innerHTML;
+        this._focusFeed();
       }
     });
 
     this.dispatchEvent(new CustomEvent('initialized', { detail: this.client }));
-  }
-
-  async _getCredentials() {
-    let res = await fetch(`${this._poolUrl}?clientId=${this._clientId}&domain=${this._domain}`);
-    res = await res.json();
-    if (!res || !res.token) {
-      throw Error('No pool users found');
-    }
-    const cred = atob(res.token).split(':');
-    this._client.setOauthConfig({client_id: cred[2]});
-    return {
-      username: cred[0],
-      password: cred[1]
-    };
   }
 
   async _connect() {
@@ -170,12 +151,7 @@ export class CircuitConversation extends HTMLElement {
       if (this._client.connectionState === 'Connected') {
         return;
       } else if (this._client.connectionState === 'Disconnected') {
-        if (this._poolUrl) {
-          const cred = await this._getCredentials();
-          await this._client.logon(cred);
-        } else {
           await this._client.logon();
-        }
       } else {
         await this._waitForConnected(5000);
       }
@@ -200,11 +176,11 @@ export class CircuitConversation extends HTMLElement {
     });
   }
 
-  async _getConversation() {
+  async _getItems() {
     try {
         await this._connect();
         this._conversation = await this._client.getConversationById(this._convId);
-        let feed = await this._client.getConversationItems(this._conversation.convId, { numberOfItems: this._initNumOfItems > this._maxItems ? this._maxItems : this._initNumOfItems });
+        let feed = await this._client.getConversationItems(this._conversation.convId, { numberOfItems: this._initNumOfItems });
         feed = feed.filter(f => f.type === Circuit.Enums.ConversationItemType.TEXT).reverse();
         this._feed = feed.reverse();
         this._renderFeed();
@@ -229,9 +205,12 @@ export class CircuitConversation extends HTMLElement {
       this._conversationTitle.innerHTML = this._conversation.topic && this._conversation.topic.length ? this._conversation.topic : this._conversation.topicPlaceholder;
       this._conversationFeed.display = 'block';
       this._input.display = 'block';
-      this.root.getElementById(this._feed[this._feed.length -1].itemId).scrollIntoView();
       // Emit event that conversation feed is loaded
-      this.dispatchEvent(new CustomEvent('loaded', { detail: this._conversation }));
+      this.dispatchEvent(new CustomEvent('loaded', { detail: { 
+        conv: this._conversation, 
+        focus: () =>  this._focusFeed()
+        } 
+      }));
   }
 
   // Takes in an item and returns the inner html for the conversation feed
@@ -257,6 +236,10 @@ export class CircuitConversation extends HTMLElement {
     }
   }
 
+  _focusFeed() {
+    this.root.getElementById(this._feed[this._feed.length -1].itemId).scrollIntoView();
+  }
+
   // Reset values if conversation changes
   _resetValues() {
     this._conversation = null;
@@ -270,9 +253,8 @@ export class CircuitConversation extends HTMLElement {
     // Non-watched attributes
     this._clientId = this.getAttribute('clientId');
     this._domain = this.getAttribute('domain') || 'circuitsandbox.net';
-    this._poolUrl = this.getAttribute('poolUrl');
     this._convId = this.getAttribute('convId');
-    this._maxItems = this.getAttribute('maxItems');
+    this._sendOnEnter = this.getAttribute('sendOnEnter');
     this._initNumOfItems = this.getAttribute('initNumOfItems');
   }
 }
