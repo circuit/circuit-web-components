d5285146cc70eb2f63dbc345d79d563ddafa1ffe
Roger Urscheler <rurscheler@gmail.com>
2019-11-22 11:43:21 -0500

    autoplay and playsinline

--- circuit-call-stage.js
+++ circuit-call-stage.js
@@ -25,8 +25,8 @@ template.innerHTML = `
 </style>
 
 <div id="container">
-  <video id="remoteVideo" muted playsinline></video>
-  <video id="localVideo" muted playsinline></video>
+  <video id="remoteVideo" muted autoplay playsinline></video>
+  <video id="localVideo" muted autoplay playsinline></video>
 </div>
 `;
 
